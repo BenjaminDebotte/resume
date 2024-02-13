a2e654e25d14d41d8e70093d5eaf7d9e9ce200a6
Benjamin Debotte <benjamin.debotte@gmail.com>
2024-02-13 10:08:39 +0100

    resume: adds jsonresume and generated exports

---
+++ index.js
@@ -0,0 +1,11 @@
+import { promises as fs } from 'fs'
+import puppeteer from 'puppeteer'
+
+const html = await fs.readFile('resume.html', 'utf-8')
+
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+
+await page.setContent(html, { waitUntil: 'networkidle0' })
+await page.pdf({ path: 'resume.pdf', printBackground: true })
+await browser.close()
